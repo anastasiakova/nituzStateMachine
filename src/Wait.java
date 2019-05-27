c7808acd7c2eea144911f9c06d643d5f1cd4d9a2
anastasiakova <kovalkov@post.bgu.ac.il>
2019-05-27 17:26:58 +0300

    all files

---
+++ src/Wait.java
@@ -0,0 +1,71 @@
+public class Off implements State {
+    @java.lang.Override
+    public void turnOn() {
+
+    }
+
+    @java.lang.Override
+    public void tunrOff() {
+
+    }
+
+    @java.lang.Override
+    public void internetOn() {
+
+    }
+
+    @java.lang.Override
+    public void internetOff() {
+
+    }
+
+    @java.lang.Override
+    public void fileRequest() {
+
+    }
+
+    @java.lang.Override
+    public void downloadAborted() {
+
+    }
+
+    @java.lang.Override
+    public void downloadError() {
+
+    }
+
+    @java.lang.Override
+    public void errorFixed() {
+
+    }
+
+    @java.lang.Override
+    public void movieOn() {
+
+    }
+
+    @java.lang.Override
+    public void restartMovie() {
+
+    }
+
+    @java.lang.Override
+    public void holdMovie() {
+
+    }
+
+    @java.lang.Override
+    public void movieOff() {
+
+    }
+
+    @java.lang.Override
+    public void resume() {
+
+    }
+
+    @java.lang.Override
+    public void downloadFinished() {
+
+    }
+}
